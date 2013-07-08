a8b393b8d5a571b5ff7cac7c3035f02be81d474a
Nada Amin <namin@alum.mit.edu>
2013-07-08 07:34:28 -0700

    add show to each fib iteration

--- fib-auto.scm
+++ fib-auto.scm
@@ -1,7 +1,9 @@
 (define fib
   (lambda (n)
-    (if (or (= n 0) (= n 1))
-        n
-        (+ (fib (- n 1)) (fib (- n 2))))))
+    (begin
+      (show n)
+      (if (or (= n 0) (= n 1))
+          n
+          (+ (fib (- n 1)) (fib (- n 2)))))))
 
 (show (fib 5))
--- fib-direct.scm
+++ fib-direct.scm
@@ -1,6 +1,8 @@
 (let ()
   (define (fib n)
-    (if (or (= n 0) (= n 1)) n
-        (+ (fib (- n 1)) (fib (- n 2)))))
+    (begin
+      (show n)
+      (if (or (= n 0) (= n 1)) n
+          (+ (fib (- n 1)) (fib (- n 2))))))
 
   (show (fib 5)))
